f3abc926c97c8574a6f534019623472be0ed1556
Uotani Satoshi <s.uotani.zetakansu@gmail.com>
2021-01-04 08:19:11 +0000

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([{name: "旅行"},{name: "プログラミング"},{name: "転職"},{name: "趣味"},{name: "日記"}])
